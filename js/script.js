880ab417336715e25f5d62722036a98003c6ec4a
ArkhipovKI <75221293+ArkhipovKI@users.noreply.github.com>
2021-01-19 21:18:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-
 // init Swiper:
 
 
-// let fullSlider = document.querySelector('.button-all');
+let fullSlider = document.querySelector('.button-all');
 
-// let text = fullSlider.textContent;
-// let butn = document.getElementById('btn');
-let slider = document.querySelector('.swiper-container');
-
+let text = fullSlider.textContent;
+let butn = document.getElementById('btn');
+let slider = document.querySelector('.slider-container');
+let wrapper = document.querySelector('.swiper-wrapper');
 let mySwiper;
 
 function mobileSlider() {
-	if (window.innerWidth <= 600 && slider.dataset.mobile == "false") {
+	if (window.innerWidth <= 700 && slider.dataset.mobile == "false") {
 		mySwiper = new Swiper(slider, {
-			slidesPerView: 1,
+			slidesPerView: 'auto',
 			spaceBetween: 10,
 			loop: true,
+			keyboard: {
+				enabled: true,
+			},
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			}
 		});
 		slider.dataset.mobile = "true";
 		console.log(slider.dataset.mobile);
 	}
 
-	if (window.innerWidth > 600) {
+	if (window.innerWidth > 700) {
 		slider.dataset.mobile = "false";
 		if (slider.classList.contains('swiper-container-initialized')) {
 			mySwiper.destroy();
@@ -35,22 +41,22 @@ window.addEventListener('resize', function () {
 	mobileSlider();
 });
 
-// fullSlider.addEventListener('click', function () {
-// 	let toggle = slider.classList.toggle('slider-full');
-// 	if (toggle) {
-// 		fullSlider.textContent = 'Скрыть всё';
-// 		rotateArrow();
-// 	}
-// 	else {
-// 		fullSlider.textContent = text;
-// 		butn.className = "";
-// 	}
-// });
+fullSlider.addEventListener('click', function () {
+	let toggle = slider.classList.toggle('slider-full');
+	if (toggle) {
+		fullSlider.textContent = 'Скрыть всё';
+		rotateArrow();
+	}
+	else {
+		fullSlider.textContent = text;
+		butn.className = "";
+	}
+});
 
-// let rotateArrow = function () {
-// 	butn.className = "rotated";
-// 	console.log(butn);
-// };
+let rotateArrow = function () {
+	butn.className = "rotated";
+	console.log(butn);
+};
 
 
 
